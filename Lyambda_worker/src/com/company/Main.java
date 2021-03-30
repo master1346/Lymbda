d67030c69640ad78e726c0a29e49ece03b156579
master1346 <shedrin.yakudza46@gmail.com>
2021-03-30 13:34:40 +0300

    Commit 0

---
+++ Lyambda_worker/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorCallback = System.out::println;
+
+        Worker worker = new Worker(listener, errorCallback);
+        worker.start();
+    }
+}
